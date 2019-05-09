1094c4b391ad564014627a9cc941597c03eecd5c
Michael Reed <michael@m-reed.com>
2019-05-08 18:19:52 -0600

    Updated index.php file

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Digital Sign-In Sheet</title>
+	<title>The Digital Sign-In Sheet</title>
     
           <meta name="viewport" content="width=device-width, initial-scale=1">
 	<style>
