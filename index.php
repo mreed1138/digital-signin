60d2ddd66e1b2d528dc58cded1322a7261985f90
Michael Reed <michael@m-reed.com>
2019-05-08 18:16:01 -0600

    updated index.php

--- index.php
+++ index.php
@@ -15,11 +15,6 @@
           <meta name="viewport" content="width=device-width, initial-scale=1">
 	<style>
 
-		/* http://meyerweb.com/eric/tools/css/reset/ 
-   v2.0 | 20110126
-   License: none (public domain)
-*/
-
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
